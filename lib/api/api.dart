74917f21160101176a207f908f43cc70dc29e521
Louis <louisreynard919@gmail.com>
2022-06-02 11:31:56 +0200

    :sparkles: api fetch working

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,16 +1,16 @@
-// import 'dart:convert';
+import 'dart:convert';
 
-// import 'package:app/models/weather.dart';
-// import 'package:http/http.dart' as http;
+import 'package:app/models/weather.dart';
+import 'package:http/http.dart' as http;
 
-// Future<weather> fetchWeatherData(String city) async {
-//   const apiKey = 'b6907d289e10d714a6e88b30761fae22';
-//   String url =
-//       'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=${apiKey}&units=metric';
-//   final response = await http.get(Uri.parse(url));
-//   if (response.statusCode == 200) {
-//     return weather.fromJson(json.decode(response.body));
-//   } else {
-//     throw Exception('Failed to load weather data ❌');
-//   }
-// }
+Future<WeatherData> fetchWeatherData(String city) async {
+  const apiKey = '856822fd8e22db5e1ba48c0e7d69844a';
+  String url =
+      'https://api.openweathermap.org/data/2.5/weather?q=London&appid=856822fd8e22db5e1ba48c0e7d69844a&units=metric';
+  final response = await http.get(Uri.parse(url));
+  if (response.statusCode == 200) {
+    return WeatherData.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load weather data ❌');
+  }
+}
